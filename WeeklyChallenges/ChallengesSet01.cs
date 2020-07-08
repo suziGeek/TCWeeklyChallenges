4daa3733a0b1b445b9e17705ebbe23d877e6cd17
Suzanne <46833370+suziGeek@users.noreply.github.com>
2020-07-07 20:50:54 -0400

    Week one challenge done

--- WeeklyChallenges/ChallengesSet01.cs
+++ WeeklyChallenges/ChallengesSet01.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata;
 
 namespace WeeklyChallenges
 {
@@ -18,32 +19,35 @@ namespace WeeklyChallenges
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+           return  minuend - subtrahend;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 < number2 ? number1 : number2;
+            
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            return nameOfPerson == "" ? "Hello!" : $"Hello, {nameOfPerson}!";         
+            
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            
+            return "HEY!";
         }
     }
 }
