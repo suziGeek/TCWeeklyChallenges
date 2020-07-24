bd1c013b74c9ae5ce9f64a01387701e152833ca8
Suzanne <46833370+suziGeek@users.noreply.github.com>
2020-07-24 11:14:36 -0400

    finished challenges set 3

--- WeeklyChallenges/ChallengesSet03.cs
+++ WeeklyChallenges/ChallengesSet03.cs
@@ -48,24 +48,39 @@ namespace WeeklyChallenges
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if(divisor == 0)
+            { return 0; }
+            return dividend/divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            var last = nums.Length-1;
+
+            var first = nums[0];
+
+            var sum = nums[last] - first;
+
+            return sum;
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            List<int> odds = new List<int>();
+
+            for (int x = 1; x < 100; x = x + 2)
+                { odds.Add(x); }
+
+            return odds.ToArray();
         }
 
-        public void ChangeAllElementsToUppercase(string[] words)
+        public string[] ChangeAllElementsToUppercase(string[] words)
         {
-            string myWords = words.ToString();
-           var yourWords =  myWords.ToUpper();
-            return yourWords;
+
+            for (int i = 0; i < words.Length; i++)
+            { words[i] = words[i].ToUpper(); }
+
+            return words;
         }
     }
 }
